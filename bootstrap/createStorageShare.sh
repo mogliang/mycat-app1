810a68a56065d44a6d5e4b7ea68b62890ba2fc04
mogliang <mogliang@gmail.com>
2022-04-27 20:18:32 +0800

    update title

--- bootstrap/createStorageShare.sh
+++ bootstrap/createStorageShare.sh
@@ -1,9 +1,16 @@
+
+
 # https://docs.microsoft.com/en-us/azure/aks/azure-files-volume
 # Change these four parameters as needed for your own environment
 AKS_PERS_STORAGE_ACCOUNT_NAME=qliangaksapp1
 AKS_PERS_RESOURCE_GROUP=qliang-aks
 AKS_PERS_LOCATION=eastus
 AKS_PERS_SHARE_NAME=qliangaksapp1share
+
+AKS_KUBECONFIG_PATH=
+AKS_NAMESPACE=app1
+
+###################### Setup file storage #########################
 
 # Create a resource group
 az group create --name $AKS_PERS_RESOURCE_GROUP --location $AKS_PERS_LOCATION
@@ -20,6 +27,11 @@ az storage share create -n $AKS_PERS_SHARE_NAME --connection-string $AZURE_STORA
 # Get storage account key
 STORAGE_KEY=$(az storage account keys list --resource-group $AKS_PERS_RESOURCE_GROUP --account-name $AKS_PERS_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
 
-# Echo storage account name and key
-echo Storage account name: $AKS_PERS_STORAGE_ACCOUNT_NAME
-echo Storage account key: $STORAGE_KEY
+# Create AKS secret
+kubectl create secret generic app1storage-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY -n $AKS_NAMESPACE
+
+
+# setup VIP
+# https://docs.microsoft.com/en-us/azure/aks/static-ip
+az network public-ip create --resource-group MC_qliang-aks_qliang-dev-aks_eastus --name app1-lb --sku Standard --allocation-method static
+VIP=$(az network public-ip show --resource-group MC_qliang-aks_qliang-dev-aks_eastus --name app1-lb --query ipAddress --output tsv)
