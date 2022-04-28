1fb8280bbbc11e21a14d26923367095a3862cf81
mogliang <mogliang@gmail.com>
2022-04-28 20:36:03 +0800

    Revert "convert to metadata.json"
    This reverts commit 086d04f8f1cfa45cc4e97bbea1485ab8c9a962db.

--- pipelines/scripts/load-env-variable.sh
+++ pipelines/scripts/load-env-variable.sh
@@ -1,8 +1,10 @@
-echo start loading environment variables
+# https://github.com/mikefarah/yq#readme
+wget https://github.com/mikefarah/yq/releases/download/v4.24.5/yq_linux_amd64 -O ~/yq &&\
+    chmod +x ~/yq
 
-cat $(Build.SourcesDirectory)/kustomize/metadata.json
+cat $(Build.SourcesDirectory)/kustomize/metadata.yaml'
 
-kubeConnection=$(jq ".environments[] | select(.name==\"$(DeploymentEnvironment)\") | .kubernetes.aksName" $(Build.SourcesDirectory)/kustomize/metadata.json)
+kubeConnection=$(~/yq ".environments[] | select(.name==\"$(DeploymentEnvironment)\") | .kubernetes.aksName" $(Build.SourcesDirectory)/kustomize/metadata.yaml')
 
 # set pipeline variable kubeConnection
 echo "##vso[task.setvariable variable=kubeConnection;isreadonly=true]$kubeConnection"
