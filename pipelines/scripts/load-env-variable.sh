f4e300fbc458b210ea4beb60f62b0436a264409b
mogliang <mogliang@gmail.com>
2022-04-28 21:45:34 +0800

    test10

--- pipelines/scripts/load-env-variable.sh
+++ pipelines/scripts/load-env-variable.sh
@@ -1,12 +1,5 @@
 # https://github.com/mikefarah/yq#readme
-wget https://github.com/mikefarah/yq/releases/download/v4.24.5/yq_linux_amd64 -O ~/yq &&\
-    chmod +x ~/yq
-
-cat ${BUILD_SOURCESDIRECTORY}/kustomize/metadata.yaml
-
+wget https://github.com/mikefarah/yq/releases/download/v4.24.5/yq_linux_amd64 -O ~/yq && chmod +x ~/yq
 kubeConnection=$(~/yq ".environments[] | select(.name==\"${DEPLOYENVIRONMENT}\") | .kubernetes.aksName" ${BUILD_SOURCESDIRECTORY}/kustomize/metadata.yaml)
-
-# set pipeline variable kubeConnection
-echo 
 echo "set pipeline variable: kubeConnection=${kubeConnection}"
 echo "##vso[task.setvariable variable=kubeConnection;isoutput=true]$kubeConnection"
