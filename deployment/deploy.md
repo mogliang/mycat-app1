f7ef868215ca74e11b14e7fff83ea3f64b9e39be
mogliang <mogliang@gmail.com>
2022-04-29 10:02:40 +0800

    update folder structure

---
+++ deployment/deploy.md
@@ -0,0 +1,28 @@
+# setup new deploy environment
+check bootstrap script
+
+# deploy deployment manually
+## build image
+check app1.fe/dockerReadMe.md
+
+## deploy
+`kubectl apply -f deployment.yaml`
+
+## check
+`
+kubectl describe deployment app1-deployment -n app1
+kubectl get pod -n app1
+kubectl logs <pod name>
+kubectl get service -n app1
+`
+
+## ssh and check mount
+```
+kubectl exec --stdin --tty <pod name> -n app1 -- /bin/bash
+cd /mnt/azure
+ls
+exit
+```
+
+## sometimes, you need a restart
+`kubectl rollout restart deployment app1-deployment -n app1`
--- pipelines/scripts/load-env-variable.sh
+++ pipelines/scripts/load-env-variable.sh
@@ -1,5 +1,5 @@
 # https://github.com/mikefarah/yq#readme
 wget https://github.com/mikefarah/yq/releases/download/v4.24.5/yq_linux_amd64 -O ~/yq && chmod +x ~/yq
-kubeConnection=$(~/yq ".environments[] | select(.name==\"${DEPLOYENVIRONMENT}\") | .kubernetes.aksName" ${BUILD_SOURCESDIRECTORY}/kustomize/metadata.yaml)
+kubeConnection=$(~/yq ".environments[] | select(.name==\"${DEPLOYENVIRONMENT}\") | .kubernetes.aksName" ${BUILD_SOURCESDIRECTORY}/deployment/kustomize/metadata.yaml)
 echo "set pipeline variable: kubeConnection=${kubeConnection}"
 echo "##vso[task.setvariable variable=kubeConnection;isoutput=true]$kubeConnection"
